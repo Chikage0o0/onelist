9309ddf0614f4fb93b27932d0955af27db4beb75
Chikage <chikage@939.me>
2024-05-09 17:24:15 +0800

    finish proxy download

--- src/web/download.rs
+++ src/web/download.rs
@@ -1,14 +1,16 @@
 use std::sync::Arc;
 
 use axum::{
+    body::Body,
     extract::{Path, State},
-    response::{IntoResponse, Redirect},
+    response::{IntoResponse, Redirect, Response},
     routing::get,
     Json,
 };
+
 use onedrive_api::{ItemId, ItemLocation};
 use serde_json::json;
-use snafu::Snafu;
+use snafu::{ResultExt, Snafu};
 
 use crate::DRIVE;
 
@@ -25,23 +27,63 @@ async fn download_file(
 
     let cache = state.download_cache.clone();
     let cached_url = cache.get(&id);
-    if let Some(url) = cached_url {
-        return Redirect::to(&url).into_response();
-    }
+    let url = if let Some(url) = cached_url {
+        url
+    } else {
+        let url = drive
+            .load()
+            .drive
+            .get_item_download_url(ItemLocation::from_id(&ItemId(id.clone())))
+            .await;
 
-    let url = drive
-        .load()
-        .drive
-        .get_item_download_url(ItemLocation::from_id(&ItemId(id.clone())))
-        .await;
-
-    match url {
-        Ok(url) => {
-            cache.insert(id, url.clone());
-            Redirect::to(&url).into_response()
+        match url {
+            Ok(url) => {
+                cache.insert(id, url.clone());
+                url
+            }
+            Err(e) => return Error::GetDownloadUrl { source: e }.into_response(),
         }
-        Err(e) => Error::GetDownloadUrl { source: e }.into_response(),
+    };
+
+    Redirect::to(&url).into_response()
+}
+
+async fn proxy_download_file(
+    State(state): State<Arc<AppState>>,
+    Path(id): Path<String>,
+) -> impl IntoResponse {
+    let drive = match DRIVE.get() {
+        Some(drive) => drive,
+        None => return Error::StillStarting.into_response(),
+    };
+
+    let cache = state.download_cache.clone();
+    let cached_url = cache.get(&id);
+    let url = if let Some(url) = cached_url {
+        url
+    } else {
+        let url = drive
+            .load()
+            .drive
+            .get_item_download_url(ItemLocation::from_id(&ItemId(id.clone())))
+            .await;
+
+        match url {
+            Ok(url) => {
+                cache.insert(id, url.clone());
+                url
+            }
+            Err(e) => return Error::GetDownloadUrl { source: e }.into_response(),
+        }
+    };
+
+    let req = reqwest::get(&url).await.context(ProxyDownloadSnafu);
+    if req.is_err() {
+        return req.unwrap_err().into_response();
     }
+    let body = req.unwrap().bytes_stream();
+
+    Response::new(Body::from_stream(body)).into_response()
 }
 
 #[derive(Debug, Snafu)]
@@ -52,6 +94,9 @@ enum Error {
 
     #[snafu(display("Failed to get the download url: {}", source))]
     GetDownloadUrl { source: onedrive_api::Error },
+
+    #[snafu(display("Failed to proxy the download: {}", source))]
+    ProxyDownload { source: reqwest::Error },
 }
 
 impl IntoResponse for Error {
@@ -66,7 +111,8 @@ impl IntoResponse for Error {
 
 pub fn router(state: Arc<AppState>) -> axum::Router {
     let route = axum::Router::new()
-        .route("/:id", get(download_file))
+        .route("/raw/:id", get(download_file))
+        .route("/proxy/:id", get(proxy_download_file))
         .with_state(state);
 
     axum::Router::new().nest("/download", route)
