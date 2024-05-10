1a417518172e23ea8e2d2b00b6de198f92bac8b8
Chikage <chikage@939.me>
2024-05-10 17:37:37 +0800

    feat: Update dependencies and add mime_guess module
    更新依赖项并添加mime_guess模块

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -1,7 +1,14 @@
 use std::{sync::Arc, time::Duration};
 
-use axum::Router;
+use axum::{
+    http::Uri,
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
 use mini_moka::sync::Cache;
+use reqwest::{header, StatusCode};
+use rust_embed::RustEmbed;
 use tokio::signal;
 use tracing::info;
 
@@ -56,7 +63,13 @@ fn router(config: Setting) -> Router {
     let router = Router::new()
         .merge(list::router(state.clone()))
         .merge(download::router(state.clone()));
-    Router::new().nest("/api", router)
+
+    Router::new()
+        .nest("/api", router)
+        .route("/", get(index_handler))
+        .route("/index.html", get(index_handler))
+        .route("/dist/*file", get(static_handler))
+        .fallback_service(get(index_handler))
 }
 
 async fn shutdown_signal() {
@@ -80,5 +93,47 @@ async fn shutdown_signal() {
     tokio::select! {
         _ = ctrl_c => {},
         _ = terminate => {},
+    }
+}
+
+// We use static route matchers ("/" and "/index.html") to serve our home
+// page.
+async fn index_handler() -> impl IntoResponse {
+    static_handler("/index.html".parse::<Uri>().unwrap()).await
+}
+
+// We use a wildcard matcher ("/dist/*file") to match against everything
+// within our defined assets directory. This is the directory on our Asset
+// struct below, where folder = "examples/public/".
+async fn static_handler(uri: Uri) -> impl IntoResponse {
+    let mut path = uri.path().trim_start_matches('/').to_string();
+
+    if path.starts_with("dist/") {
+        path = path.replace("dist/", "");
+    }
+
+    StaticFile(path)
+}
+
+#[derive(RustEmbed)]
+#[folder = "ui/dist"]
+struct Asset;
+
+pub struct StaticFile<T>(pub T);
+
+impl<T> IntoResponse for StaticFile<T>
+where
+    T: Into<String>,
+{
+    fn into_response(self) -> Response {
+        let path = self.0.into();
+
+        match Asset::get(path.as_str()) {
+            Some(content) => {
+                let mime = mime_guess::from_path(path).first_or_octet_stream();
+                ([(header::CONTENT_TYPE, mime.as_ref())], content.data).into_response()
+            }
+            None => (StatusCode::NOT_FOUND, "404 Not Found").into_response(),
+        }
     }
 }
